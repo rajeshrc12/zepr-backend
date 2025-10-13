453b044381ecb9007b569b3df1809779ce1221ce
rajeshrc12 <rajesh.charhajari@gmail.com>
2025-10-13 17:45:10 +0530

    code cleanup

--- app/models/task.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-from sqlmodel import SQLModel, Field
-
-
-class Task(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
-    is_completed: bool = False
--- app/models/user.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-from sqlmodel import SQLModel, Field
-
-
-class User(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
-    email: str = Field(index=True, unique=True)
-    image: Optional[str] = None  # URL to profile picture
